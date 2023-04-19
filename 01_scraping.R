1267de6243fc5029b6a4270d023c17f84e388db2
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-19 11:58:21 +0200

    Query parameters

--- 01_scraping.R
+++ 01_scraping.R
@@ -4,3 +4,8 @@ library(guardianapi)
 api_key <- rstudioapi::askForPassword()
 Sys.setenv(GU_API_KEY = api_key) 
 gu_api_key(check_env = TRUE)
+
+# Query parameters
+query<- c("vegan", "paleo", "keto")
+from_date <- "2000-01-01"
+to_date <- "2023-04-19"
