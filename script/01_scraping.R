ae746468f2d48af883e5e131cc0fe7e4fd86cfee
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-19 14:08:01 +0200

    Create a new list with common columns

--- script/01_scraping.R
+++ script/01_scraping.R
@@ -1,28 +1,28 @@
 library(guardianapi)
 
-# set API key
+#set API key
 api_key <- rstudioapi::askForPassword()
 Sys.setenv(GU_API_KEY = api_key) 
 gu_api_key(check_env = TRUE)
 
-# Query parameters
+#Query parameters
 query<- c("vegan", "paleo", "keto")
 from_date <- "2000-01-01"
 to_date <- "2023-04-19"
 
-# Get articles for each query
+#Get articles for each query
 articles <- list()
 for (q in query) {
   res <- gu_content(apiKey = api_key,
                     query = q,
                     from = from_date,
                     to = to_date)
-# Add a column indicating the diet type
+#Add a column indicating the diet type
   res$diet_found <- q
   articles[[q]] <- res
 }
 
-#column names
+#Column names
 col_names <- lapply(articles, colnames)
 
 #Checking common columns
@@ -33,4 +33,11 @@ for (df_name in names(articles)) {
     message(paste0("The following columns are in ", df_name, " but not in the other data frames: ", 
                    paste(extra_cols, collapse = ", ")))
   }
+}
+
+#Create new list with common columns
+newArticles <- list()
+for (i in 1:length(articles)) {
+  new_list = subset(articles[[i]],TRUE, common_cols)
+  newArticles[[i]]= new_list
 }
