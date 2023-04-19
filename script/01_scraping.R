fada440894214fb517f1ec0b651284d7397258c3
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-19 12:43:42 +0200

    Scraped "vegan" into df

--- script/01_scraping.R
+++ script/01_scraping.R
@@ -1,4 +1,5 @@
 library(guardianapi)
+library(stringr)
 
 # set API key
 api_key <- rstudioapi::askForPassword()
@@ -6,18 +7,14 @@ Sys.setenv(GU_API_KEY = api_key)
 gu_api_key(check_env = TRUE)
 
 # Query parameters
-query<- c("vegan", "paleo", "keto")
 from_date <- "2000-01-01"
 to_date <- "2023-04-19"
 
 # Get articles for each query
-articles <- list()
-for (q in query) {
-  res <- gu_content(apiKey = api_key,
-                    query = q,
-                    from = from_date,
-                    to = to_date)
-  # Add a column indicating the diet type
-  res$diet_found <- q
-  articles[[q]] <- res
-}
+
+res_vegan<- gu_content(apiKey = api_key,
+                      query = "vegan",
+                      from = from_date,
+                      to = to_date)
+ 
+
